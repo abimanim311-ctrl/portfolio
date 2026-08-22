910f4563d87a41e26ee6941fbc7f77972274fa07
abimanim311-ctrl <abimanim311@gmail.com>
2026-08-22 19:02:12 +0530

    Refactor ContactController to remove unused repository
    Removed unused ContactRepository dependency and related save logic.

--- src/main/java/com/portfolio/controller/ContactController.java
+++ src/main/java/com/portfolio/controller/ContactController.java
@@ -1,7 +1,6 @@
 package com.portfolio.controller;
 
 import com.portfolio.model.ContactMessage;
-import com.portfolio.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,17 +12,10 @@ import org.springframework.web.bind.annotation.*;
 public class ContactController {
 
     @Autowired
-    private ContactRepository contactRepository;
-
-    @Autowired
     private JavaMailSender mailSender;
 
     @PostMapping("/send")
     public String sendMessage(@RequestBody ContactMessage message) {
-        // 1. Save to Database
-        contactRepository.save(message);
-
-        // 2. Dispatch Email (catch exception so that email configuration failures don't crash form processing)
         try {
             SimpleMailMessage mail = new SimpleMailMessage();
             mail.setTo("abimanim311@gmail.com");
